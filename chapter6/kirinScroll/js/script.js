bfc17bfea1f3138c056ea3729aa11df6da13948c
ToothieWn <tasuke8@gmail.com>
2024-11-24 16:32:44 +0900

    page254

---
+++ chapter6/kirinScroll/js/script.js
@@ -0,0 +1,13 @@
+const kirin = document.querySelector('#kirin');
+
+const showKirin = (entries) => {
+  const keyframes = {
+    opacity: [0, 1],
+    translate: ['200px 0', 0],
+  };
+  entries[0].target.animate(keyframes, 600);
+};
+
+const kirinObserver = new IntersectionObserver(showKirin);
+
+kirinObserver.observe(kirin);
